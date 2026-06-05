ee55925620978856101c3f69459c92485f0827b6
onuurdogan5 <onurefedogan5@gmail.com>
2026-06-05 22:25:41 +0300

    Add files via upload

---
+++ Hafta3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hafta3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adsoyad = "Onur Efe Doğan";
+            Console.WriteLine(adsoyad);
+        }
+    }
+}
