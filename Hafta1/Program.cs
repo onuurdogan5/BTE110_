475948f4e0c4f6377fda8479a29e3377427fc8f9
onuurdogan5 <onurefedogan5@gmail.com>
2026-06-05 22:21:57 +0300

    Add files via upload

---
+++ Hafta1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hafta1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
